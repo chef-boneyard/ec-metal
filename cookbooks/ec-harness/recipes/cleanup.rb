23388854a37dd4ed2ee6218318f8f288d714cb48
Irving Popovetsky <irving@getchef.com>
2014-03-26 17:39:33 -0700

    Bring frontends up as well

--- cookbooks/ec-harness/recipes/cleanup.rb
+++ cookbooks/ec-harness/recipes/cleanup.rb
@@ -1,10 +1,11 @@
 # encoding: utf-8
 include_recipe "ec-harness::vagrant"
 
-node['harness']['vm_config']['backends'].each do |backend,config|
+node['harness']['vm_config']['backends'].merge(
+  node['harness']['vm_config']['frontends']).each do |vmname, config|
 
   # Bring up our backend machines
-  machine backend do
+  machine vmname do
 
     action :delete
   end
--- cookbooks/ec-harness/recipes/private_chef_ha.rb
+++ cookbooks/ec-harness/recipes/private_chef_ha.rb
@@ -1,25 +1,38 @@
 # encoding: utf-8
-HOST_CACHE_PATH = node['harness']['host_cache_path']
 
-node['harness']['vm_config']['backends'].each do |backend,config|
+# This is fucking gross and horrible, make me a library SOON
+def generate_vagrant_config(vmname, config)
+  # Vagrant/Virtualbox notes:
+  # * it sucks that you have to hardcode "IDE Controller", recent opscode
+  #   packer images switched to IDE, but we can't easily detect SATA
+  # * virtio network interfaces, in some circumstances, provide MUCH WORSE
+  #   performance than good ol' e1000 (the default)
+  # * What's the point of the "nonrotational" flag?  tells you the underlying
+  #   disk is an SSD.  This should be fine for most of our recent Macs, but I'm
+  #   not sure if there's any actual benefit for ext4
 
-  vm_disk2 = ::File.join(node['harness']['vms_dir'], backend, 'disk2.vmdk')
-  # This is fucking gross and horrible, make me a library SOON
   vagrant_config = <<-ENDCONFIG
     config.vm.network 'private_network', ip: "#{config['ipaddress']}"
-    config.vm.network 'private_network', ip: "#{config['cluster_ipaddress']}"
     config.vm.hostname = "#{config['hostname']}"
-    config.vm.synced_folder "#{HOST_CACHE_PATH}", '/tmp/cache'
+    config.vm.synced_folder "#{node['harness']['host_cache_path']}", '/tmp/cache'
     config.vm.provider 'virtualbox' do |v|
       v.customize [
         'modifyvm', :id,
-        '--name', "#{backend}",
+        '--name', "#{vmname}",
         '--memory', "#{config['memory']}",
         '--cpus', "#{config['cpus']}",
         '--natdnshostresolver1', 'on',
         '--usb', 'off',
         '--usbehci', 'off'
       ]
+    end
+  ENDCONFIG
+
+  if node['harness']['vm_config']['backends'].include?(vmname)
+    vm_disk2 = ::File.join(node['harness']['vms_dir'], vmname, 'disk2.vmdk')
+    vagrant_config += <<-ENDCONFIG
+    config.vm.network 'private_network', ip: "#{config['cluster_ipaddress']}"
+    config.vm.provider 'virtualbox' do |v|
       v.customize ['createhd',
                   '--filename', "#{vm_disk2}",
                   '--size', 2 * 1024,
@@ -32,27 +45,24 @@ node['harness']['vm_config']['backends'].each do |backend,config|
                   '--nonrotational', 'on',
                   '--medium', "#{vm_disk2}"]
     end
-  ENDCONFIG
+    ENDCONFIG
+  end
+  vagrant_config
+end
 
-  # Vagrant/HW notes:
-  # * it sucks that you have to hardcode "IDE Controller", recent opscode
-  #   packer images switched to IDE, but we can't easily detect SATA
-  # * virtio network interfaces, in some circumstances, provide MUCH WORSE
-  #   performance than good ol' e1000 (the default)
-  # * What's the point of the "nonrotational" flag?  tells you the underlying
-  #   disk is an SSD.  This should be fine for most of our recent Macs, but I'm
-  #   not sure if there's any actual benefit for ext4
+
+# Bring the backends and frontends online
+node['harness']['vm_config']['backends'].merge(
+  node['harness']['vm_config']['frontends']).each do |vmname, config|
 
   node_attributes = {
     'private-chef' => node['harness']['vm_config'],
     'root_ssh' => node['harness']['root_ssh'].to_hash
   }
 
+  vagrant_config = generate_vagrant_config(vmname, config)
 
-
-  # Bring up our backend machines
-  machine backend do
-
+  machine vmname do
     local_provisioner_options = {
       'vagrant_config' => vagrant_config,
     }
@@ -61,17 +71,9 @@ node['harness']['vm_config']['backends'].each do |backend,config|
     attributes node_attributes
 
     recipe 'private-chef::provision'
-    recipe 'private-chef::drbd'
+    recipe 'private-chef::drbd' if node['harness']['vm_config']['backends'].include?(vmname)
     recipe 'private-chef::provision_phase2'
-
-    if node['harness']['vm_config']['backends'][backend]['bootstrap'] == true
-      puts "***** Configuring node #{backend} as the bootstrap and primary backend"
-    else
-      puts "***** Configuring node #{backend} as the standby backend"
-    end
 
     action :create
   end
-
 end
-
