05062563b2e55a85e56c435def149fa15a3d13a0
Irving Popovetsky <irving@getchef.com>
2014-04-09 20:59:36 -0700

    WIP

---
+++ cookbooks/ec-harness/recipes/ec2.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+require 'cheffish'
+require 'chef_metal_fog'
+
+repo_path = node['harness']['repo_path']
+
+# with_chef_local_server :chef_repo_path => repo_path,
+#   :cookbook_path => [ File.join(repo_path, 'cookbooks'),
+#     File.join(repo_path, 'vendor', 'cookbooks') ]
+
+with_fog_ec2_provisioner # :provider => 'AWS'
+
+with_provisioner_options :image_id => node['harness']['ec2']['ami_id']
+
+fog_key_pair 'me' do
+  private_key_path File.join(repo_path, 'keys', 'ec2_key')
+  public_key_path File.join(repo_path, 'keys', 'ec2_key.pub')
+end
+
+# set provisioner options for all of our machines
+node['harness']['vm_config']['backends'].merge(
+  node['harness']['vm_config']['frontends']).each do |vmname, config|
+
+  local_provisioner_options = {
+  }
+
+  node.set['harness']['provisioner_options'][vmname] = ChefMetal.enclosing_provisioner_options.merge(local_provisioner_options)
+
+end
