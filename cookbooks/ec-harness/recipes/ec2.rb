eeff9927bede003588b43845738f52cc4119c856
Irving Popovetsky <irving@getchef.com>
2014-04-15 17:36:19 -0700

    Get to the point where actual machines are launched at EC2. Note that for some reason only works in us-east-1

--- cookbooks/ec-harness/recipes/ec2.rb
+++ cookbooks/ec-harness/recipes/ec2.rb
@@ -5,13 +5,17 @@ require 'chef_metal_fog'
 
 repo_path = node['harness']['repo_path']
 
-# with_chef_local_server :chef_repo_path => repo_path,
-#   :cookbook_path => [ File.join(repo_path, 'cookbooks'),
-#     File.join(repo_path, 'vendor', 'cookbooks') ]
+with_chef_local_server :chef_repo_path => repo_path,
+  :cookbook_path => [ File.join(repo_path, 'cookbooks'),
+    File.join(repo_path, 'vendor', 'cookbooks') ]
 
-with_fog_ec2_provisioner # :provider => 'AWS'
+with_fog_ec2_provisioner :ssh_username => node['harness']['ec2']['ssh_username']
 
-with_provisioner_options :image_id => node['harness']['ec2']['ami_id']
+with_provisioner_options :bootstrap_options => {
+    'image_id' => node['harness']['ec2']['ami_id'],
+    'flavor_id' => node['harness']['ec2']['backend_instance_type'],
+    'region' => node['harness']['ec2']['region']
+  }
 
 fog_key_pair 'me' do
   private_key_path File.join(repo_path, 'keys', 'ec2_key')
