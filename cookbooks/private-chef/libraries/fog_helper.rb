b2623bb5c0c5dab126e891633e1c3dc5a3adc139
Irving Popovetsky <irving@getchef.com>
2014-05-01 10:20:42 -0700

    Support the region argument

--- cookbooks/private-chef/libraries/fog_helper.rb
+++ cookbooks/private-chef/libraries/fog_helper.rb
@@ -3,9 +3,12 @@
 
 class FogHelper
 
-  def self.aws_vpc_assign_secondary_ip(aws_access_key_id, aws_secret_access_key, server_id, ipaddress)
+  def self.aws_vpc_assign_secondary_ip(provider, region, aws_access_key_id, aws_secret_access_key, server_id, ipaddress)
     require 'fog'
-    compute = Fog::Compute.new( :aws_access_key_id => aws_access_key_id, :aws_secret_access_key => aws_secret_access_key, :provider => 'AWS')
+    compute = Fog::Compute.new( :aws_access_key_id => aws_access_key_id,
+      :aws_secret_access_key => aws_secret_access_key,
+      :region => region,
+      :provider => provider)
     backend1 = compute.servers.get(server_id)
     myeni = backend1.network_interfaces.first['networkInterfaceId']
     compute.assign_private_ip_addresses(myeni, 'PrivateIpAddresses' => [ipaddress], 'AllowReassignment' => true)
--- cookbooks/private-chef/providers/backend_vip.rb
+++ cookbooks/private-chef/providers/backend_vip.rb
@@ -5,10 +5,11 @@
 action :create do
   aws_access_key_id = node['cloud']['aws_access_key_id']
   aws_secret_access_key = node['cloud']['aws_secret_access_key']
+  region = node['cloud']['region']
   server_id = node['provisioner_output']['server_id']
   ipaddress = node['private-chef']['backend_vip']['ipaddress']
 
-  FogHelper.aws_vpc_assign_secondary_ip(aws_access_key_id,aws_secret_access_key,server_id,ipaddress)
+  FogHelper.aws_vpc_assign_secondary_ip('AWS', region, aws_access_key_id, aws_secret_access_key, server_id, ipaddress)
   log "Assigned backend_vip #{ipaddress} to server #{server_id}"
 end
 
