c453a5a65aaab01cf4577ef36d1509814cf56c8a
Irving Popovetsky <irving@getchef.com>
2014-06-03 15:56:42 -0700

    New knife options to tell chef-metal where the ssh keys are

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,13 @@
-current_dir = File.dirname(__FILE__)
+current_dir = ::File.dirname(__FILE__)
+harness_dir = ::File.expand_path(::File.join(current_dir, '..'))
+keys_dir = ::File.join(harness_dir, 'keys')
 log_level                :info
 log_location             STDOUT
 node_name                "metal-mastah"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            [::File.join(current_dir, '..', 'cookbooks')]
+cookbook_path            [::File.join(harness_dir, 'cookbooks')]
 verify_api_cert          true
+private_key_paths	 [keys_dir]
+private_keys		 "#{ENV['USER']}@#{::File.basename(harness_dir)}" => ::File.join(keys_dir, 'id_rsa')
+public_keys		 "#{ENV['USER']}@#{::File.basename(harness_dir)}" => ::File.join(keys_dir, 'id_rsa.pub')
