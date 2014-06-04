25d1a043c15a69051691a327f2e217a995258b17
Irving Popovetsky <irving@getchef.com>
2014-06-04 15:07:23 -0700

    Automatically hunt for an open chef-zero TCP port

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,3 +1,15 @@
+def find_open_port
+  port = 8889
+  begin
+    s = TCPServer.new('127.0.0.1', port)
+    s.close
+  rescue
+    port += 1
+    retry
+  end
+  port
+end
+
 current_dir = ::File.dirname(__FILE__)
 harness_dir = ::File.expand_path(::File.join(current_dir, '..'))
 keys_dir = ::File.join(harness_dir, 'keys')
@@ -11,3 +23,4 @@ verify_api_cert          true
 private_key_paths	 [keys_dir]
 private_keys		 "#{ENV['USER']}@#{::File.basename(harness_dir)}" => ::File.join(keys_dir, 'id_rsa')
 public_keys		 "#{ENV['USER']}@#{::File.basename(harness_dir)}" => ::File.join(keys_dir, 'id_rsa.pub')
+chef_zero		 :port => find_open_port
