8b78c25bf01eee7cd720689b4287db79631fc113
Irving Popovetsky <irving@getchef.com>
2014-06-11 17:14:05 -0700

    Better OC-11382 fix

--- cookbooks/private-chef/recipes/bugfixes.rb
+++ cookbooks/private-chef/recipes/bugfixes.rb
@@ -44,3 +44,14 @@ cookbook_file '/opt/opscode/embedded/cookbooks/enterprise/definitions/component_
   subscribes :create, "package[#{installer_name}]", :immediately
   action :nothing
 end
+
+# OC-11382
+cookbook_file '/opt/opscode/embedded/service/omnibus-ctl/upgrade.rb' do
+  source 'upgrade.rb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  only_if { PackageHelper.private_chef_installed_version(node).match('^11.') && node['private-chef']['backends'][node.name] }
+  subscribes :create, "package[#{installer_name}]", :immediately
+  action :nothing
+end
