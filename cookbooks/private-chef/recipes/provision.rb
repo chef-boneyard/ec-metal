2a9b078910ffb4db3668453dbadfb250d620e38e
Irving Popovetsky <irving@getchef.com>
2014-06-26 10:04:29 -0700

    Fix sudo error on RHEL-platform on EC2

--- cookbooks/private-chef/recipes/provision.rb
+++ cookbooks/private-chef/recipes/provision.rb
@@ -91,9 +91,17 @@ file '/root/.ssh/config' do
   content "Host *\n  StrictHostKeyChecking no\n"
 end
 
-# Deal with RHEL-based boxes that may have their own firewalls up
+# RHEL-specific bug fixes
 if node['platform_family'] == 'rhel'
+  # Deal with RHEL-based boxes that may have their own firewalls up
   service 'iptables' do
     action [ :disable, :stop ]
+  end
+
+  # As of EC11.1.8, we need to disable sudo 'requiretty' on RHEL-based systems
+  execute 'sudoers-disable-requiretty' do
+    command 'sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers'
+    action :run
+    only_if 'grep "^Defaults.*requiretty" /etc/sudoers'
   end
 end
