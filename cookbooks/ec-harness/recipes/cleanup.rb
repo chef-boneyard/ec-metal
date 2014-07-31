2d5f1ff5638dd9bdfce64f67771c6bc790e50a6a
Irving Popovetsky <irving@getchef.com>
2014-07-31 15:30:21 -0700

    Finally a proper cleanup for the EBS secondary volume and databag

--- cookbooks/ec-harness/recipes/cleanup.rb
+++ cookbooks/ec-harness/recipes/cleanup.rb
@@ -20,6 +20,49 @@ if node['harness']['provider'] == 'ec2' && node['harness']['ec2']['backend_stora
         Chef::Log.info("Databag ebs_volumes_db not found, continuing")
       end
 
+      # destroy the volume (scary!)
+      fog_helper = FogHelper.new(region: node['harness']['ec2']['region'])
+      begin
+        fog_helper.get_aws.delete_volume(ebs_vol_id)
+      rescue Fog::Compute::AWS::Error => e
+        # typically due to volume still attached, machine shutting down
+        Chef::Log.info("AWS error, sleeping and then retrying: #{e}")
+        sleep 5
+        retry
+      rescue Fog::Compute::AWS::NotFound => e
+        # because the volume doesn't exist
+        Chef::Log.info("Volume not found, continuing")
+      end
+    end
+  end
+
+  ruby_block 'delete_ebs_volumes_databag_item' do
+    block do
+      topology = TopoHelper.new(ec_config: node['harness']['vm_config'])
+      begin
+        item = data_bag_item('ebs_volumes_db', topology.bootstrap_host_name)
+        item.destroy
+      rescue Net::HTTPServerException
+        Chef::Log.info("Databag ebs_volumes_db not found, continuing")
+      end
+    end
+  end
+
+end
+if node['harness']['provider'] == 'ec2' && node['harness']['ec2']['backend_storage_type'] == 'ebs'
+
+  ruby_block 'destroy_ebs_volume' do
+    block do
+      # get the EBS data volume id
+      topology = TopoHelper.new(ec_config: node['harness']['vm_config'])
+
+      begin
+        item = data_bag_item('ebs_volumes_db', topology.bootstrap_host_name)
+        ebs_vol_id = item['volume_id']
+      rescue Net::HTTPServerException
+        Chef::Log.info("Databag ebs_volumes_db not found, continuing")
+      end
+
       if ebs_vol_id
         fog_helper = FogHelper.new(region: node['harness']['ec2']['region'])
         begin
