f6ec0e55d4aede90152c6774bc575c58a0fc85c7
Irving Popovetsky <irving@getchef.com>
2014-08-05 08:28:35 -0700

    Rebase onto master, fixing merge conflicts

---
+++ cookbooks/docker_host/metadata.rb
@@ -0,0 +1,9 @@
+name             'docker_host'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures docker_host'
+long_description 'Installs/Configures docker_host'
+version          '0.1.0'
+
+depends 'docker'
