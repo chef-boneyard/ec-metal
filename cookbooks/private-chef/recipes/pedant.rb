ace14a70d5e2a146c0c3f80448a6471a2bef5412
Ho-Sheng Hsiao <hosh@getchef.com>
2014-08-13 16:37:29 -0700

    Add default_orgname tests

--- cookbooks/private-chef/recipes/pedant.rb
+++ cookbooks/private-chef/recipes/pedant.rb
@@ -7,3 +7,13 @@ execute 'run pedant' do
   action :run
   only_if { node['run-pedant'] }
 end
+
+execute 'run pedant in default-org mode' do
+  if node['osc-install']
+    fail '--default-org mode is not available for OSC'
+  else
+    command '/opt/opscode/bin/private-chef-ctl test --use-default-org'
+  end
+  action :run
+  only_if { node['run-pedant'] && node['private-chef']['default_orgname'] }
+end
