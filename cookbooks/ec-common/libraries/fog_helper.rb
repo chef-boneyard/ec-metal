7277cc8aa6bd1df1791c3b46045177f4420584dc
Irving Popovetsky <irving@getchef.com>
2014-08-12 18:17:43 -0700

    Add ELB and IAM capabilities to the fog_helper, also allow it to be called from within the nodes easier

--- cookbooks/ec-common/libraries/fog_helper.rb
+++ cookbooks/ec-common/libraries/fog_helper.rb
@@ -2,11 +2,41 @@
 
 class FogHelper
 
-  attr_accessor :ami, :region
+  attr_accessor :ami, :region, :credentials
 
   def initialize(params = {})
     @ami = params[:ami] || 'ami-XXXXXXX'
-    @region = params[:region] || 'us-east-1'
+    @region = params[:region] || nil
+    @credentials = params[:credentials] || load_ini
+  end
+
+  def create_elb(id, subnet_id)
+    @elb ||= get_elb
+    @elb.load_balancers.create(
+      id: id,
+      listeners: [
+        {
+          protocol: 'HTTP',
+          lb_port: 80,
+          instance_protocol: 'HTTP',
+          instance_port: '80'
+        }
+      ],
+      subnet_ids: [subnet_id]
+    )
+  end
+
+  def get_az_of_subnet(subnet_id)
+    @aws ||= get_aws
+    @aws
+      .subnets
+      .get(subnet_id)
+      .availability_zone
+  end
+
+  def elb_exists?(id)
+    @elb ||= get_elb
+    @elb.load_balancers.get(id) ? true : false
   end
 
   def get_root_blockdevice
@@ -17,8 +47,8 @@ class FogHelper
   end
 
   def describe_ami
-    aws = get_aws
-    aws.describe_images('ImageId' => @ami).body['imagesSet']
+    @aws ||= get_aws
+    @aws.describe_images('ImageId' => @ami).body['imagesSet']
   end
 
   # refactor me
@@ -33,17 +63,28 @@ class FogHelper
     compute.assign_private_ip_addresses(myeni, 'PrivateIpAddresses' => [ipaddress], 'AllowReassignment' => true)
   end
 
+  def get_iam
+    require 'fog'
+    Fog::AWS::IAM.new(:aws_access_key_id => @credentials[:aws_access_key_id],
+      :aws_secret_access_key => @credentials[:aws_secret_access_key])
+  end
+
+  def get_elb
+    require 'fog'
+    Fog::AWS::ELB.new(:aws_access_key_id => @credentials[:aws_access_key_id],
+      :aws_secret_access_key => @credentials[:aws_secret_access_key],
+      :region => @region || @credentials[:region])
+  end
+
   def get_aws
     require 'fog'
-    aws_credentials = load_ini('~/.aws/config')
-
-    Fog::Compute.new(:aws_access_key_id => aws_credentials['default'][:access_key_id],
-      :aws_secret_access_key => aws_credentials['default'][:secret_access_key],
-      :region => @region || aws_credentials['default'][:region],
+    Fog::Compute.new(:aws_access_key_id => @credentials[:aws_access_key_id],
+      :aws_secret_access_key => @credentials[:aws_secret_access_key],
+      :region => @region || @credentials[:region],
       :provider => 'AWS')
   end
 
-  def load_ini(credentials_ini_file)
+  def load_ini(credentials_ini_file='~/.aws/config', ini_profile='default')
     require 'inifile'
     credentials = {}
     inifile = IniFile.load(File.expand_path(credentials_ini_file))
@@ -51,13 +92,13 @@ class FogHelper
       if section =~ /^\s*profile\s+(.+)$/ || section =~ /^\s*(default)\s*/
         profile = $1.strip
         credentials[profile] = {
-          :access_key_id => inifile[section]['aws_access_key_id'],
-          :secret_access_key => inifile[section]['aws_secret_access_key'],
+          :aws_access_key_id => inifile[section]['aws_access_key_id'],
+          :aws_secret_access_key => inifile[section]['aws_secret_access_key'],
           :region => inifile[section]['region']
         }
       end
     end
-    credentials
+    credentials[ini_profile]
   end
 
 end
