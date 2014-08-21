8d8d23820042f41fc762faca873cfe8b00d6ede3
Irving Popovetsky <irving@getchef.com>
2014-08-21 14:13:02 -0700

    Switch loadtester_host to run loadtester_guest in containers

--- cookbooks/loadtester_host/metadata.rb
+++ cookbooks/loadtester_host/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          ''
 description      'Installs/Configures docker_host'
 long_description 'Installs/Configures docker_host'
-version          '0.2.1'
+version          '0.3.0'
 
 depends 'docker'
 depends 'ohai'
