dfc4ad8ead6740fb62f6f2d97531a5eb5d4ab3fe
Irving Popovetsky <irving@getchef.com>
2014-08-21 14:12:03 -0700

    Add the loadtester_guest recipe, which is run within the containers

---
+++ cookbooks/loadtester_guest/metadata.rb
@@ -0,0 +1,9 @@
+name             'loadtester_guest'
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures loadtester_guest'
+long_description 'Installs/Configures loadtester_guest'
+version          '0.2.1'
+
+depends 'chef-client'
