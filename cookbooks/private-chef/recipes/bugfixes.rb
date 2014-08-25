363805cfcc6e5efab494ad53dda2dd0bd6c23145
Irving Popovetsky <irving@getchef.com>
2014-08-25 16:22:54 -0700

    Force bugfixes to 11.1.x only,  11.2 has moved past most of these

--- cookbooks/private-chef/recipes/bugfixes.rb
+++ cookbooks/private-chef/recipes/bugfixes.rb
@@ -57,7 +57,7 @@ end
 #   owner 'root'
 #   group 'root'
 #   mode '0644'
-#   only_if { PackageHelper.private_chef_installed_version(node).match('^11.') && topology.is_backend?(node.name) }
+#   only_if { PackageHelper.private_chef_installed_version(node).match('^11.1') && topology.is_backend?(node.name) }
 #   subscribes :create, "package[#{installer_name}]", :immediately
 #   action :nothing
 # end
