040d8651c9d1b35ded939e1b1f3ae035f68685d9
Patrick Wright <patrick@getchef.com>
2014-08-26 10:28:00 -0700

    refactor rhel specific resources

--- cookbooks/private-chef/recipes/provision.rb
+++ cookbooks/private-chef/recipes/provision.rb
@@ -88,18 +88,3 @@ file '/root/.ssh/config' do
   mode '0600'
   content "Host *\n  StrictHostKeyChecking no\n"
 end
-
-# RHEL-specific bug fixes
-if node['platform_family'] == 'rhel'
-  # Deal with RHEL-based boxes that may have their own firewalls up
-  service 'iptables' do
-    action [ :disable, :stop ]
-  end
-
-  # As of EC11.1.8, we need to disable sudo 'requiretty' on RHEL-based systems
-  execute 'sudoers-disable-requiretty' do
-    command 'sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers'
-    action :run
-    only_if 'grep "^Defaults.*requiretty" /etc/sudoers'
-  end
-end
