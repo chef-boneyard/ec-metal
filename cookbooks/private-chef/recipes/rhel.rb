76e70bb76ad89b6106d878c697cfd5683e518a76
Patrick Wright <patrick@getchef.com>
2014-08-26 10:28:00 -0700

    refactor rhel specific resources

---
+++ cookbooks/private-chef/recipes/rhel.rb
@@ -0,0 +1,19 @@
+remote_file "/tmp/packages@opscode.com.gpg.key" do
+  source "http://apt.opscode.com/packages@opscode.com.gpg.key"
+end
+
+execute "rpm --import /tmp/packages\@opscode.com.gpg.key"
+
+directory "/etc/cron.d"
+
+# Deal with RHEL-based boxes that may have their own firewalls up
+service 'iptables' do
+  action [ :disable, :stop ]
+end
+
+# As of EC11.1.8, we need to disable sudo 'requiretty' on RHEL-based systems
+execute 'sudoers-disable-requiretty' do
+  command 'sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers'
+  action :run
+  only_if 'grep "^Defaults.*requiretty" /etc/sudoers'
+end
