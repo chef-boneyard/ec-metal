dc5c4b45c2c021d4d3f65457714e4d623eff8037
Irving Popovetsky <irving@getchef.com>
2014-09-11 17:00:33 -0700

    change default checkin interval to every 15 minutes

--- cookbooks/loadtester_guest/attributes/default.rb
+++ cookbooks/loadtester_guest/attributes/default.rb
@@ -4,7 +4,7 @@ default['chef_client']['splay']  = '300'
 default['chef_client']['daemon_options'] = []
 default['chef_client']['cron']['use_cron_d'] = true
 default['chef_client']['cron']['log_file'] = '/var/log/chef-client.log'
-default['chef_client']['cron']['minute'] = '*/30' # every 30 minutes
+default['chef_client']['cron']['minute'] = '*/15' # every 30 minutes
 default['chef_client']['cron']['hour'] = '*'
 
 # start cron in foreground mode because runit
