16999f8defa5dc92317a9f801829fa3b99013183
Irving Popovetsky <irving@getchef.com>
2015-03-27 09:06:56 -0700

    Test fixes for ec-tools and private-chef coobkooks

--- cookbooks/private-chef/attributes/default.rb
+++ cookbooks/private-chef/attributes/default.rb
@@ -9,19 +9,24 @@
 # High level options
 ###
 
-default['private-chef']['installer_file'] = nil
-default['private-chef']['analytics_installer_file'] = nil
-default['private-chef']['reporting_installer_file'] = nil
-default['private-chef']['manage_installer_file'] = nil
+default['private-chef']['installer_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/chef-server-core_12.0.5-1_amd64.deb'
+default['analytics']['analytics_installer_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/opscode-analytics_1.1.1-1_amd64.deb'
+default['private-chef']['reporting_installer_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/opscode-reporting_1.2.3-1_amd64.deb'
+default['private-chef']['manage_installer_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/opscode-analytics_1.1.1-1_amd64.deb'
+default['private-chef']['pushy_installer_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/opscode-analytics_1.1.1-1_amd64.deb'
 
 default['private-chef']['topology'] = "standalone"
 default['private-chef']['api_fqdn'] = "api.mycompany.com"
 default['private-chef']['manage_fqdn'] = "manage.mycompany.com"
 
+default['root_ssh']['privkey'] = ''
+default['root_ssh']['pubkey'] = ''
+
 ###
 # Underlying Configuration for /etc/opscode/chef-server.rb
 ###
 default['private-chef']['configuration'] = {}
+default['private-chef']['manage_options'] = {}
 
 # wrapping the aws cookbook
 default['aws']['right_aws_version'] = '3.1.0'
