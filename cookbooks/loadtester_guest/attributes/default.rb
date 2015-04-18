e23279ef566bf90d4944da3f509bd7b2d3f3c64f
Irving Popovetsky <irving@getchef.com>
2015-04-18 08:10:04 -0700

    Make push jobs non-default, and go back to chef-client daemon because overlayfs

--- cookbooks/loadtester_guest/attributes/default.rb
+++ cookbooks/loadtester_guest/attributes/default.rb
@@ -1,16 +1,27 @@
 
 # chef-client cookbook overrides
-default['chef_client']['splay']  = '300'
-default['chef_client']['daemon_options'] = []
-default['chef_client']['cron']['use_cron_d'] = true
-default['chef_client']['cron']['log_file'] = '/var/log/chef-client.log'
-default['chef_client']['cron']['hour'] = '*'
-default['chef_client']['cron']['minute'] = '*/30'
-default["chef_client"]["splay"] = 1800
+default['chef_client']['splay']  = 1800
+default['chef_client']['interval'] = 1800
+# default['chef_client']['daemon_options'] = []
+# default['chef_client']['cron']['use_cron_d'] = true
+# default['chef_client']['cron']['log_file'] = '/var/log/chef-client.log'
+# default['chef_client']['cron']['hour'] = '*'
+# default['chef_client']['cron']['minute'] = '*/30'
 
 # start cron in foreground mode because runit
 default['container_service']['cron']['command'] = "/usr/sbin/cron -f -L15"
+# default['container_service']['chef-client']['command'] = "chpst -P /usr/bin/chef-client -d -i #{node['chef_client']['interval']} -s #{node['chef_client']['splay']}"
 default['container_service']['opscode-push-jobs-client']['command'] = "/opt/opscode-push-jobs-client/bin/pushy-client -c /etc/chef/push-jobs-client.rb"
 
 # set environment
 default['loadtester_guest']['chef_environment'] = 'loadtest'
+
+# Push jobs settings
+default['loadtester_guest']['push_jobs_enabled'] = false
+default['push_jobs']['init_style'] = 'container'
+default['push_jobs']['package_url'] = 'https://mark-testing.s3.amazonaws.com/opscode-push-jobs-client_1.1.5-1_amd64.deb?AWSAccessKeyId=AKIAIRISJZOEYLU52WCA&Expires=1457825224&Signature=zm2qixrhrI49RrqRwl1q5%2BsfS7w%3D'
+default['push_jobs']['package_checksum'] = 'd7b40ebb18c7c7dbc32322c9bcd721279e707fd1bee3609a37055838afbf67ea'
+default['push_jobs']['whitelist'] = {
+  "chef-client" => "chef-client",
+  "sleep" => "sleep 5"
+}
--- cookbooks/loadtester_guest/recipes/default.rb
+++ cookbooks/loadtester_guest/recipes/default.rb
@@ -9,24 +9,13 @@
 # node.chef_environment = ['loadtester_guest']['chef_environment']
 
 # Run chef-client from cron.  Saves memory vs daemonized chef-client
-include_recipe 'chef-client::cron'
+include_recipe 'chef-client::default'
 
-service 'cron' do
-  action :start
+# service 'cron' do
+#   action :start
+# end
+
+# For running the push jobs client within your containers
+if node['loadtester_guest']['push_jobs_enabled'] == true
+  include_recipe 'push-jobs::default'
 end
-
-#
-#
-#
-node.set['push_jobs']['init_style'] = 'container'
-
-node.set['push_jobs']['package_url'] = 'https://mark-testing.s3.amazonaws.com/opscode-push-jobs-client_1.1.5-1_amd64.deb?AWSAccessKeyId=AKIAIRISJZOEYLU52WCA&Expires=1457825224&Signature=zm2qixrhrI49RrqRwl1q5%2BsfS7w%3D'
-node.set['push_jobs']['package_checksum'] = 'd7b40ebb18c7c7dbc32322c9bcd721279e707fd1bee3609a37055838afbf67ea'
-
-node.set['push_jobs']['whitelist'] = {
-  "chef-client" => "chef-client",
-  "sleep" => "sleep 5"
-}
-
-include_recipe 'push-jobs::default'
-
